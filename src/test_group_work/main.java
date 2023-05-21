abf4f86bff74c3c89af7020b681f1a22b2935e5f
Duc Ohlin <hoangminhduc4125@gmail.com>
2023-05-21 15:24:08 +0700

    commit2

---
+++ src/test_group_work/main.java
@@ -0,0 +1,12 @@
+package test_group_work;
+import java.util.Scanner;
+
+public class main {
+	public static void main(String [] args) {
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		
+		System.out.println("a = " + a + "b = 0" + b);
+	}
+}
